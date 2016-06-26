3b294648680c8f8405f516a96d0f2f526c3bd100
Tobi Oduah <tobi.oduah@andela.com>
2016-06-26 09:22:22 +0100

    Clean up code and add README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Angular-2048
+
+An implementation of the classic 2048 game in Angular.
+
+To build, clone project, and run:
+
+`gulp`
+
+This creates a build folder housing minified page resources ready for production. Use https://www.browsersync.io/ or any other http server for serving files to browser.
--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -20,6 +20,7 @@
       }
       $scope.gameObj = [];
       $scope.backups = [];
+      $scope.gameDone = false;
 
       for (var i = 0; i < $scope.GRID_LENGTH; i++){
         $scope.gameObj[i] = []
@@ -38,13 +39,21 @@
     function fillInColumns(numberOfTimes){
       for (var i = 0; i < numberOfTimes; i++){
         var pair = RandomGenerator.getRandompair();
-        $scope.gameObj[pair.iGrid][pair.jGrid].number = RandomGenerator.randomNumber();
+        if (pair != undefined)
+          $scope.gameObj[pair.iGrid][pair.jGrid].number = RandomGenerator.randomNumber();
+        else if (transverseForMove())
+          $scope.gameDone = true;
       }
     }
 
     function postMove(){
       fillInColumns(1);
       resetCanAdd();
+    }
+
+    function preMove(){
+      if ($scope.backups.length > 10) $scope.backups.shift();
+      $scope.backups.push(angular.copy($scope.gameObj));
     }
 
     function resetCanAdd(){
@@ -55,30 +64,48 @@
       }
     }
 
+    function transverseForMove(){
+      for (var i = 0; i < $scope.GRID_LENGTH; i++){
+        for (var j = 0; j < $scope.GRID_LENGTH - 1; j++){
+          if ($scope.gameObj[i][j] == $scope.gameObj[i][j+1])
+            return false;
+        }
+      }
+
+      for (var j = 0; j < $scope.GRID_LENGTH; j++){
+        for (var i = 0; i < $scope.GRID_LENGTH - 1; i++){
+          if ($scope.gameObj[i][j] == $scope.gameObj[i+1][j])
+            return false;
+        }
+      }
+
+      return true;
+    }
+
     $scope.moveUp = function(){
       verticalPoint = 0 + 1
-      $scope.backups.push(angular.copy($scope.gameObj));
+      preMove();
       MovementService.mover(verticalPoint, 0, verticalPoint, "up", false, $scope.gameObj, $scope.GRID_LENGTH)
       postMove();
     }
 
     $scope.moveLeft = function(){
       verticalPoint = 0 + 1
-      $scope.backups.push(angular.copy($scope.gameObj));
+      preMove();
       MovementService.mover(0, verticalPoint, verticalPoint, "left", false, $scope.gameObj, $scope.GRID_LENGTH)
       postMove();
     }
 
     $scope.moveDown = function(){
       verticalPoint = $scope.GRID_LENGTH - 2;
-      $scope.backups.push(angular.copy($scope.gameObj));
+      preMove();
       MovementService.mover(verticalPoint, 0, verticalPoint, "down", false, $scope.gameObj, $scope.GRID_LENGTH)
       postMove();
     }
 
     $scope.moveRight = function(){
       verticalPoint = $scope.GRID_LENGTH - 2;
-      $scope.backups.push(angular.copy($scope.gameObj));
+      preMove();
       MovementService.mover(0, verticalPoint, verticalPoint, "right", false, $scope.gameObj, $scope.GRID_LENGTH)
       postMove();
     }
@@ -87,7 +114,11 @@
       $scope.gameObj = $scope.backups.pop()
     }
 
-    initValues();
+    $scope.newGame = function(){
+      initValues();
+    }
+
+    // initValues();
   }
   MainCtrl.$inject = ["$scope", "$timeout", "MovementService", "RandomGenerator"]
 })();
